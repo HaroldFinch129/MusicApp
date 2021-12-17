55f96db9dc3a5f210852cb10ae9b4e75ea5856a9
Harold <m.h.gamze@gmail.com>
2021-12-17 11:11:29 +0300

    App js main skeleton occured

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import {View,Text, StyleSheet, FlatList} from 'react-native';
 import MusicAppCard from "./components/MusicAppCard/MusicAppCard";
 import music_data from './music-data.json';
+import SearchBar from "./components/MusicAppCard/SearchBar";
 
 const App = () => { 
   const renderMusic = ({item}) => <MusicAppCard market={item}/>
   return(
     <View style={styles.container}>
       <Text style={styles.headline}>MusicApp</Text>
+       <SearchBar/>
       <FlatList
       data={music_data}
       renderItem={renderMusic}
