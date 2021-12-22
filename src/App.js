45d84b7939fd0444280febf37c5f36e99015be84
Harold <m.h.gamze@gmail.com>
2021-12-22 10:10:30 +0300

    filter funcion fixed

--- src/App.js
+++ src/App.js
@@ -1,61 +1,52 @@
-import React, {useState} from "react";
-import {View,Text, StyleSheet, FlatList} from 'react-native';
-import MusicAppCard from "./components/MusicAppCard/MusicAppCard";
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, FlatList} from 'react-native';
+import MusicAppCard from './components/MusicAppCard/MusicAppCard';
 import music_data from './music-data.json';
-import SearchBar from "./components/SearchBar/SearchBar";
+import SearchBar from './components/SearchBar/SearchBar';
 
-const App = () => { 
-  const[list, setList]= useState(music_data);
-  
-  const renderMusic = ({item}) => <MusicAppCard market={item}/>
-  const renderSeperator = () => <View style={styles.seperator}></View>
+const App = () => {
+  const [list, setList] = useState(music_data);
+
+  const renderMusic = ({item}) => <MusicAppCard market={item} />;
+  const renderSeperator = () => <View style={styles.seperator}></View>;
   const handleSearch = text => {
-    const filteredList = music_data.filter(song => {
+    const filteredList = music_data.filter(market => {
       const searchedText = text.toLowerCase();
-      const currentTitle = song.title.toLowerCase();
-      currentTitle.indexOf(searchedText);
+      const currentTitle = market.title.toLowerCase();
+      return currentTitle.indexOf(searchedText) > -1;
     });
     setList(filteredList);
+    console.log(filteredList);
   };
 
-
-  
-  return(
+  return (
     <View style={styles.container}>
       <Text style={styles.headline}>MusicApp</Text>
-       <SearchBar onSearch={handleSearch}/>
-      <FlatList 
-      keyExtractor={item => item.id} 
-      data={list}
-      renderItem={renderMusic}
-      ItemSeparatorComponent={renderSeperator}
+      <SearchBar onSearch={handleSearch} />
+      <FlatList
+        keyExtractor={item => item.id}
+        data={list}
+        renderItem={renderMusic}
+        ItemSeparatorComponent={renderSeperator}
       />
-
     </View>
-  )
-}
+  );
+};
 
 export default App;
 
-const styles = StyleSheet.create(
-{
-  container:{
+const styles = StyleSheet.create({
+  container: {
     // backgroundColor:'wheat',
-    flex:1,
-    
-
-    
+    flex: 1,
   },
-  headline:{
-    color:'#151D52',
-    fontSize:30,
-    fontWeight:'bold',
-
+  headline: {
+    color: '#151D52',
+    fontSize: 30,
+    fontWeight: 'bold',
   },
-  seperator:{
-    borderWidth:1,
-    borderColor:'#efefef',
+  seperator: {
+    borderWidth: 1,
+    borderColor: '#efefef',
   },
-  
-},
-);
+});
